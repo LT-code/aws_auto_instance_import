0bd126b8416ac893dc2f20e9084068c7c268841c
root <root@sd-161610.dedibox.fr>
2021-02-15 10:47:57 +0100

    adding import script

--- import.sh
+++ import.sh
@@ -5,20 +5,14 @@
 
 #aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document "file://import/role-policy.json"
 
-## vm 111
-#qemu-img convert /mnt/isofiles/briks/images/111/vm-112-disk-0.qcow2 ./112.raw
-#aws s3 cp ./111.raw s3://vm-import-images-epitech-tcloud901/112.raw
-aws ec2 import-image --description "Master mariadb" --disk-containers "file://import/vm-111-import.json"
-
-## vm 112
-#qemu-img convert /mnt/isofiles/briks/images/112/vm-112-disk-0.qcow2 ./112.raw
-#aws s3 cp ./111.raw s3://vm-import-images-epitech-tcloud901/112.raw
-aws ec2 import-image --description "Slave 1 mariadb" --disk-containers "file://import/vm-112-import.json"
-
-## vm 113
-#qemu-img convert /mnt/isofiles/briks/images/113/vm-113-disk-0.qcow2 ./113.raw
-#aws s3 cp ./111.raw s3://vm-import-images-epitech-tcloud901/113.raw
-aws ec2 import-image --description "Slave 2 mariadb" --disk-containers "file://import/vm-113-import.json"
+for i in `seq 2 3`;
+do
+	echo "############## vm 11$i"
+	#qemu-img convert /mnt/isofiles/briks/images/11$i/vm-11$i-disk-0.qcow2 ./11$i.raw
+	aws s3 mb s3://vm-import-images-epitech-tcloud901-vm11$i --region eu-west-$i
+	aws s3 cp ./11$i.raw s3://vm-import-images-epitech-tcloud901-vm11$i/11$i.raw
+	aws ec2 import-image --region eu-west-$i --description "Mariadb $i" --disk-containers "file://import/vm-11$i-import.json"
+done
 
 # monitoring import
 #aws ec2 describe-import-image-tasks --import-task-ids import-ami-1234567890abcdef0
